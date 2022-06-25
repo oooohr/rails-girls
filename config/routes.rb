e5f14a29b944136e6115b93af56f8ea376a92870
mako <kuskus_822-m.o@outlook.jp>
2022-06-25 12:46:43 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do  
+  get 'pages/info'
+  root to: redirect('/ideas')
+  resources :ideas
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get "pages/info"
+end
